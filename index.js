9f9326537175e11eb4b9e89310b40f09dafd5609
Ivan Bozic <ivan@arsfutura.co>
2021-02-11 23:10:24 +0100

    Added the flag planting and flag assets.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+window.PLAYERS = {
+    red: {
+        flag: {
+            position: 1
+        }
+    },
+    black: {
+        flag: {
+            position: 8
+        }
+    },
+    green: {
+        flag: {
+            position: 57
+        }
+    },
+    blue: {
+        flag: {
+            position: 64
+        }
+    }
+}
+
 window.addEventListener('load', () => {
     window.BOARD = document.getElementById("board");
 
     generateTiles(8, 8);
+    plantFlags();
 });
 
 function generateTiles (width, height) {
@@ -14,7 +38,7 @@ function generateTiles (width, height) {
         for (j = 0; j < width; j++) {
             const tile = document.createElement("div");
             tile.classList.add("tile");
-            tile.dataset.index = index;
+            tile.id = `tile-${index}`;
 
             row.appendChild(tile);
             index += 1;
@@ -22,4 +46,11 @@ function generateTiles (width, height) {
 
         window.BOARD.appendChild(row);
     }
+}
+
+function plantFlags () {
+    document.getElementById(`tile-${window.PLAYERS.red.flag.position}`).classList.add("tile--flag", "flag--red");
+    document.getElementById(`tile-${window.PLAYERS.black.flag.position}`).classList.add("tile--flag", "flag--black");
+    document.getElementById(`tile-${window.PLAYERS.green.flag.position}`).classList.add("tile--flag", "flag--green");
+    document.getElementById(`tile-${window.PLAYERS.blue.flag.position}`).classList.add("tile--flag", "flag--blue");
 }
