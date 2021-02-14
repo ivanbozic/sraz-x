497d9b06312d2dca2360e0cb1ab4fbc13794b716
Ivan Bozic <ivan@arsfutura.co>
2021-02-14 16:22:06 +0100

    Players can now move only once.

--- index.js
+++ index.js
@@ -1,160 +1,5 @@
 window.DEBUG = true;
 
-window.PLAYERS = {
-    red: {
-        identifier: "red",
-        flag: {
-            position: 1
-        },
-        pieces: {
-            R1: {
-                identifier: "R1",
-                type: "pawn",
-                position: 17,
-                moves: 1
-            },
-            R2: {
-                identifier: "R2",
-                type: "pawn",
-                position: 9,
-                moves: 1
-            },
-            R3: {
-                identifier: "R3",
-                type: "pawn",
-                position: 10,
-                moves: 1
-            },
-            R4: {
-                identifier: "R4",
-                type: "pawn",
-                position: 2,
-                moves: 1
-            },
-            R5: {
-                identifier: "R5",
-                type: "pawn",
-                position: 3,
-                moves: 1
-            }
-        }
-    },
-    black: {
-        identifier: "black",
-        flag: {
-            position: 8
-        },
-        pieces: {
-            K1: {
-                identifier: "K1",
-                type: "pawn",
-                position: 24,
-                moves: 1
-            },
-            K2: {
-                identifier: "K2",
-                type: "pawn",
-                position: 16,
-                moves: 1
-            },
-            K3: {
-                identifier: "K3",
-                type: "pawn",
-                position: 15,
-                moves: 1
-            },
-            K4: {
-                identifier: "K4",
-                type: "pawn",
-                position: 7,
-                moves: 1
-            },
-            K5: {
-                identifier: "K5",
-                type: "pawn",
-                position: 6,
-                moves: 1
-            }
-        }
-    },
-    green: {
-        identifier: "green",
-        flag: {
-            position: 57
-        },
-        pieces: {
-            G1: {
-                identifier: "G1",
-                type: "pawn",
-                position: 41,
-                moves: 1
-            },
-            G2: {
-                identifier: "G2",
-                type: "pawn",
-                position: 49,
-                moves: 1
-            },
-            G3: {
-                identifier: "G3",
-                type: "pawn",
-                position: 50,
-                moves: 1
-            },
-            G4: {
-                identifier: "G4",
-                type: "pawn",
-                position: 58,
-                moves: 1
-            },
-            G5: {
-                identifier: "G5",
-                type: "pawn",
-                position: 59,
-                moves: 1
-            }
-        }
-    },
-    blue: {
-        identifier: "blue",
-        flag: {
-            position: 64
-        },
-        pieces: {
-            B1: {
-                identifier: "B1",
-                type: "pawn",
-                position: 48,
-                moves: 1
-            },
-            B2: {
-                identifier: "B2",
-                type: "pawn",
-                position: 56,
-                moves: 1
-            },
-            B3: {
-                identifier: "B3",
-                type: "pawn",
-                position: 55,
-                moves: 1
-            },
-            B4: {
-                identifier: "B4",
-                type: "pawn",
-                position: 63,
-                moves: 1
-            },
-            B5: {
-                identifier: "B5",
-                type: "pawn",
-                position: 62,
-                moves: 1
-            }
-        }
-    }
-}
-
 window.CURRENT_PLAYER = null;
 window.CURRENT_POSITION = null;
 window.MOVE_POSITION = null;
@@ -182,6 +27,8 @@ window.addEventListener('load', () => {
     window.CURRENT_POSITION = window.PLAYERS.red.pieces.R1.position;
 
     setCurrentPosition();
+
+    moveToNextPlayerIfPossible();
 });
 
 function generateTiles (width, height) {
@@ -214,6 +61,7 @@ function plantFlags () {
 function positionPieces () {
     for (const player in window.PLAYERS) {
         for (const piece in window.PLAYERS[player].pieces) {
+            window.PLAYERS[player].pieces[piece].availableMoves = window.PLAYERS[player].pieces[piece].moves;
             positionSinglePiece(window.PLAYERS[player].pieces[piece].position, piece, player);
         }
     }
@@ -386,6 +234,10 @@ function canThePieceMove(pieceIdentifier) {
     if (pieceIdentifier) {
         piece = getPieceByIdentifier(pieceIdentifier);
 
+        if (piece.availableMoves == 0) {
+            return false;
+        }
+
         // The piece can't move to a different place if there's a hole
         // in that place, if there is another piece there, if the piece
         // is on the edge of the board and is trying to move over the edge,
@@ -502,8 +354,11 @@ function movePiece(piece, newPosition) {
     clearPieceFromTile(piece.position);
 
     window.PLAYERS[piece.color].pieces[piece.identifier].position = newPosition;
+    window.PLAYERS[piece.color].pieces[piece.identifier].availableMoves = piece.moves - 1;
 
     positionSinglePiece(newPosition, piece.identifier, piece.color);
+
+    moveToNextPlayerIfPossible();
 }
 
 function clearPieceFromTile(tilePosition) {
@@ -529,7 +384,10 @@ function paintPieceProperties(piece) {
     boot.alt = "Boot";
 
     movesContainer.innerHTML = null;
-    movesContainer.appendChild(boot);
+
+    Array(piece.availableMoves).fill().map(_ => {
+        movesContainer.appendChild(boot);
+    });
 }
 
 function clearPieceProperties() {
@@ -542,10 +400,26 @@ function clearPieceProperties() {
 
 function numberToRomanNumeral(integer) {
     // This is not a complete implementation since we are
-    // certain that pieces will not go above 5.
+    // certain that pieces in Sraz will not go above 5.
     if (integer == 1) return "I";
     if (integer == 2) return "II";
     if (integer == 3) return "III";
     if (integer == 4) return "IV";
     if (integer == 5) return "V";
+}
+
+function moveToNextPlayerIfPossible() {
+    let piecesThatCanMove = 0;
+
+    for (const piece in window.CURRENT_PLAYER.pieces) {
+        if(window.CURRENT_PLAYER.pieces[piece].availableMoves > 0) {
+            piecesThatCanMove += 1;
+        }
+    }
+
+    console.log(`Pieces that can move: ${piecesThatCanMove}`);
+
+    if (piecesThatCanMove == 0) {
+        nextPlayer();
+    }
 }
---
+++ players.js
@@ -0,0 +1,154 @@
+window.PLAYERS = {
+    red: {
+        identifier: "red",
+        flag: {
+            position: 1
+        },
+        pieces: {
+            R1: {
+                identifier: "R1",
+                type: "pawn",
+                position: 17,
+                moves: 1
+            },
+            R2: {
+                identifier: "R2",
+                type: "pawn",
+                position: 9,
+                moves: 1
+            },
+            R3: {
+                identifier: "R3",
+                type: "pawn",
+                position: 10,
+                moves: 1
+            },
+            R4: {
+                identifier: "R4",
+                type: "pawn",
+                position: 2,
+                moves: 1
+            },
+            R5: {
+                identifier: "R5",
+                type: "pawn",
+                position: 3,
+                moves: 1
+            }
+        }
+    },
+    black: {
+        identifier: "black",
+        flag: {
+            position: 8
+        },
+        pieces: {
+            K1: {
+                identifier: "K1",
+                type: "pawn",
+                position: 24,
+                moves: 1
+            },
+            K2: {
+                identifier: "K2",
+                type: "pawn",
+                position: 16,
+                moves: 1
+            },
+            K3: {
+                identifier: "K3",
+                type: "pawn",
+                position: 15,
+                moves: 1
+            },
+            K4: {
+                identifier: "K4",
+                type: "pawn",
+                position: 7,
+                moves: 1
+            },
+            K5: {
+                identifier: "K5",
+                type: "pawn",
+                position: 6,
+                moves: 1
+            }
+        }
+    },
+    green: {
+        identifier: "green",
+        flag: {
+            position: 57
+        },
+        pieces: {
+            G1: {
+                identifier: "G1",
+                type: "pawn",
+                position: 41,
+                moves: 1
+            },
+            G2: {
+                identifier: "G2",
+                type: "pawn",
+                position: 49,
+                moves: 1
+            },
+            G3: {
+                identifier: "G3",
+                type: "pawn",
+                position: 50,
+                moves: 1
+            },
+            G4: {
+                identifier: "G4",
+                type: "pawn",
+                position: 58,
+                moves: 1
+            },
+            G5: {
+                identifier: "G5",
+                type: "pawn",
+                position: 59,
+                moves: 1
+            }
+        }
+    },
+    blue: {
+        identifier: "blue",
+        flag: {
+            position: 64
+        },
+        pieces: {
+            B1: {
+                identifier: "B1",
+                type: "pawn",
+                position: 48,
+                moves: 1
+            },
+            B2: {
+                identifier: "B2",
+                type: "pawn",
+                position: 56,
+                moves: 1
+            },
+            B3: {
+                identifier: "B3",
+                type: "pawn",
+                position: 55,
+                moves: 1
+            },
+            B4: {
+                identifier: "B4",
+                type: "pawn",
+                position: 63,
+                moves: 1
+            },
+            B5: {
+                identifier: "B5",
+                type: "pawn",
+                position: 62,
+                moves: 1
+            }
+        }
+    }
+}
